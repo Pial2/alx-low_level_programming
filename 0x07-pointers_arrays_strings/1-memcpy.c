556fc25caec2bc95dff8628d2273afc7c3e5c054
Pial2 <palpaul2020@gmail.com>
2022-09-26 13:26:39 -0700

    question to updadated file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memcpy -> memory copy
+ * @dest: is destination memory
+ * @src: is source memory
+ * @n: number of bytes to be copied
+ * Return: string copied from source
+ */
+char *_memcpy(char *dest, *src, unsigned int n)
+{
+    unsigned int a;
+
+  for (a = 0, a < n; a++)
+	  des[a] = src[a];
+  return (dest);
+}

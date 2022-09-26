c37077056814744ba47e91787b5c2032f9778376
Pial2 <palpaul2020@gmail.com>
2022-09-26 12:22:53 -0700

    section 11 updated file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _memset -> this memory set function
@@ -10,5 +9,10 @@
  */ 
 char *_memset(char *s, char b, unsigned int n)
 {			 
-         return (memset(s,b,n);
+    unsigned int a;
+
+    for (a = 0; a < n; a++)
+	    s[a] = b;
+
+    return (s);
 }

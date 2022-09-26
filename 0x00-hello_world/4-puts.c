ec8554d6ce2a3b97d3b9ab12d55e0164650a13a4
Pial2 <palpaul2020@gmail.com>
2022-09-26 06:21:09 -0700

    4-puts.c has been update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+include <stdio>
+/**
+ * Description: main - Prints out a string stdout,
+ * Return: 0 if success.
+ */
+ int main(void)
+ { 
+ puts ("\"Programming is like building a multilingual puzzle")
+ };
+ return (0);
+ }

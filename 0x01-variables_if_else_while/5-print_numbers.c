d61401fcc1ae34a983db53ccb087f8dc54783729
Pial2 <https://github.com/Pial2>
2022-12-12 02:23:29 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * main - Prints numbers from 0 to 9
- *
  * Return: Always 0 (Success)
  */
 
@@ -10,8 +9,8 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a < 10, a++)
-		printf ("%d", a);
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
 	putchar('\n');
 	return (0);
 }

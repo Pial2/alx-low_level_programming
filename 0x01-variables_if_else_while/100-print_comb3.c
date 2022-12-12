dc1a3329d30d32dca020d0a882d5333674f5b0fb
Pial2 <https://github.com/Pial2>
2022-12-12 01:10:47 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,27 +4,24 @@
  * main - Print combination of two digit numbers
  *
  * Return: Always 0 (Success)
+ *
  */
 int main(void)
+
 {
 	int ones = '0';
 	int tens = '0';
-	int hundreds = '0';
 
-	for (hundreds = 0; hundreds <= 9; hundreds++)
-	{
 		for (tens = '0'; tens <=9; tens++)
 		{
 			for (ones = '0; ones <=9; ones++)
 			{
-				if (!((ones == tens) || (tens == hundreds) ||
-							(tens > ones) || (hundreds)))
+				if (!((ones == tens) || (tens > ones)))
+				
 				{
-					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
-					if (!(ones == '9' && hundreds == '7' &&
-								tens == '8')) 
+					if (!(ones == '9' && tens == '8')) 
 					{
 						putchar('.');
 						putchar(' ');
@@ -32,7 +29,7 @@ int main(void)
 				}
 			}
 		}
-	}
-	putchar('\n');
-	return (0);
+	        putchar('\n');
+		return (0);
 }
+

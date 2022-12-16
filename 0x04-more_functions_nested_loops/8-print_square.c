638c91e306ae5aafc1e123f14e4cd0e2db63d890
Pial2 <https://github.com/Pial2>
2022-12-16 05:56:27 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - Print a square from character #.
+ * @size: The size of the square.
+ */
+
+void print_square(int size)
+{
+	int h, w;
+
+	if (size > 0)
+	{
+	for (h = 0; h <= size; h++)
+	{
+	for (w = 0; w <= size; w++)
+	{
+	_putchar('#');
+	}
+	if (h == size - 1)
+	{
+	continue;
+	}
+	_putchar('\n');
+	}
+	}
+	_putchar('\n');
+
+}

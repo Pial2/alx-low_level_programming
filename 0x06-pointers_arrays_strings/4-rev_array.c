1b8479e14a150bdcfa0e917485327aef76f1ddf3
Pial2 <https://github.com/Pial2>
2022-12-22 07:41:05 -0800

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,15 +4,17 @@
  * reverse-array - Reverses the content of an array of integers.
  * @: The array of integers to be reversed.
  * @n: The number of elements in the array.
+ *
  */
+
 void reverse_array(int *a, int n)
 {
 	int temp, index;
 
 	for (index = n - 1; index >= n / 2; index--)
 	{
-		temp = a[n - 1 - index];
-		a[n - 1 - index] = a[index];
-		a[index] = tmp;
+	temp = a[n - 1 - index];
+	a[n - 1 - index] = a[index];
+	a[index] = temp;
 	}
 }

473de609788a353d493ede4a1253a21083b0e35e
Pial2 <https://github.com/Pial2>
2022-12-22 06:17:00 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,24 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: (pialvangoonerpeople)
- * Desc: Header file containing prototypes foll all functions
- *       used in the 0x06. c - More pointers, arrays and strings directory.
- */
-int _putchar(char c);
+#include <stdio.h>
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char  *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a. int n);
-char *strring_toupper(char *);
+char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
-char *infinite_add(char n1, char *n2, char *, int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
 #endif

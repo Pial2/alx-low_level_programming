de6c78994f3032760d847d474b28d85a96e9dad7
Pial2 <https://github.com/Pial2>
2022-12-23 07:57:32 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,45 +1,28 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string using rot13
- * @str: the string targeted
+ * rot13 - function that encodes a string using rot13
+ * @s: string
  * Return: return the encoded string
  */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
-	int indx1, indx2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 
-		             'G', 'H', 'I', 'J', 'K', 'L', 
-			     'M', 'N', 'O', 'P', 'Q', 'R', 
-			     'S', 'T', 'U', 'V', 'W', 'X', 
-			     'Y', 'Z', 'a', 'b', 'c', 'd',
-                             'e', 'f', 'g', 'h', 'i', 'j',
-			     'k', 'l', 'm', 'n', 'o', 'p',
-			     'q', 'r', 's', 't', 'u', 'v',
-			     'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-		             'T', 'U', 'V', 'W', 'X', 'Y',
-			     'Z', 'A', 'B', 'C', 'D', 'E',
-			     'F', 'G', 'H', 'I', 'J', 'K',
-			     'L', 'M', 'n', 'o', 'p', 'q',
-			     'r', 's', 't', 'u', 'v', 'w',
-			     'x', 'y', 'z', 'a', 'b', 'c',
-			     'd', 'e', 'f', 'g', 'h', 'i',
-			     'j', 'k', 'l', 'm'};
-	
-	while (str[indx2])
+	int x, y;
+
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";		
+	for (x = 0; s[x] != '\0'; x++)
 	{
-	        for (indx2 = 0; index2 < 52; index2++)
+		for (y = 0; input[y] != '\0'; y++)
 		{
-			if (str[index1] == alphabet[index2])
+			if (s[x] == input[y])
 			{
-				str[index1] = rot13key[index2];
+				s[x] = output[y];
 				break;
 			}
 		}
-
-		index1++;
 	}
-	return (str);
+	return (s);
 }

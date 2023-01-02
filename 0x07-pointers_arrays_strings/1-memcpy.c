58dd19bf202826c2af4ddf08033beefcd151035c
Pial2 <https://github.com/Pial2>
2023-01-02 03:18:47 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,7 +10,7 @@
  * Return: A pointer to the destination buffer @dest.
  */
 
-void *_memcpy(void *dest, const void *src, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
 	unsigned char *destination = dest;
@@ -18,7 +18,5 @@ void *_memcpy(void *dest, const void *src, size_t n)
 
 	for (index = 0; index < n; index++)
 		destination[index] = source[index];
-	
-
 	return (dest);
 }

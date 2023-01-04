37054d64fc6f268c760e3154e452c37043771508
Pial2 <https://github.com/Pial2>
2023-01-04 02:58:37 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -21,3 +21,20 @@ return (-1);
 
 return (-1);
 }
+/**
+ * _sqrt_recursion - evaluate sqrt
+ * @n: integer
+ * Return: sqrt_recursion
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0)
+		return (-1);
+	else
+	{
+		return (_evaluate(i, n));
+	}
+}

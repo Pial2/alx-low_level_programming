41d70de23e43dec4861150276a107acb220725fe
Pial2 <https://github.com/Pial2>
2023-01-09 01:50:09 -0800

    1-strdup.c1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,34 +1,83 @@
 #include "main.h"
+#include <stddef.h>
 #include <stdlib.h>
 
 /**
- * _strdup - returns pointer allocated space, memory string passed.
- * @str: pointer to string being duplicated.
- *
- * Return: NULL if str is NULL.
- * pointer to duplicated string on success.
- * NULL if memory was insufficient.
+ * _strlen - count array
+ * @s: array of elements
+ * Return: 1
+ */
+int _strlen(char *s)
+{
+unsigned int i;
+
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+
+return (i);
+}
+
+/**
+ * _strcpy - copy arrays
+ * @src: array of elements
+ * @dest: dest array
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+}
+
+/**
+ * _strdup - array for prints a string
+ * @str: array of elements
+ * Return: pointer
  */
 char *_strdup(char *str)
 {
-	char *nstr;
-	unsigned int len, i;
+char *dst;
+unsigned int size;
 
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-
-	len = 0;
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-
-	for (i = 0; i < len; i++)
-	{
-		nstr[i] = str[i];
-	}
-	nstr[len] = '\0';
-	return (nstr);
+if (str == 0)
+{
+return (NULL);
 }
+
+size = _strlen(str) + 1;
+
+dst = (char *) malloc(size *sizeof(char));
+
+if (dst == 0)
+{
+return (NULL);
+}
+_strcpy(dst, str);
+return (dst);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
